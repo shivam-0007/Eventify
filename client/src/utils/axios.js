cb1da5fde88318eec4b35a1b03068c4097b93d42
shivam-0007 <shivamkumarkatras91@gmail.com>
2026-07-10 17:36:53 +0530

    Update axios baseURL to include '/api' path

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL: "https://eventify-server-y3cd.onrender.com",
+    baseURL: "https://eventify-server-y3cd.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
