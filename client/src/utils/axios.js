41f848ac4c26e05498d09ca3ff673c94a266a969
shivam-0007 <shivamkumarkatras91@gmail.com>
2026-07-10 17:24:38 +0530

    Change API base URL to production server

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://eventify-server-y3cd.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
